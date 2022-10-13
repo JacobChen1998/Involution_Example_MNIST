45fd5d8f7161ad7c39d932fd994f9c81398ce07e
Jing-Yao Chen (Jacob) <68951355+JacobChen1998@users.noreply.github.com>
2022-10-13 23:55:14 +0800

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,108 @@
+import tensorflow.keras as keras
+import tensorflow as tf
+
+
+class Involution(keras.layers.Layer):
+    def __init__(
+        self, channel, group_number, kernel_size, stride, reduction_ratio, name
+    ):
+        super().__init__(name=name)
+
+        # Initialize the parameters.
+        self.channel = channel
+        self.group_number = group_number
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.reduction_ratio = reduction_ratio
+
+    def build(self, input_shape):
+        # Get the shape of the input.
+        (_, height, width, num_channels) = input_shape
+
+        # Scale the height and width with respect to the strides.
+        height = height // self.stride
+        width = width // self.stride
+
+        # Define a layer that average pools the input tensor
+        # if stride is more than 1.
+        self.stride_layer = (
+            keras.layers.AveragePooling2D(
+                pool_size=self.stride, strides=self.stride, padding="same"
+            )
+            if self.stride > 1
+            else tf.identity
+        )
+        # Define the kernel generation layer.
+        self.kernel_gen = keras.Sequential(
+            [
+                keras.layers.Conv2D(
+                    filters=self.channel // self.reduction_ratio, kernel_size=1
+                ),
+                keras.layers.BatchNormalization(),
+                keras.layers.ReLU(),
+                keras.layers.Conv2D(
+                    filters=self.kernel_size * self.kernel_size * self.group_number,
+                    kernel_size=1,
+                ),
+            ]
+        )
+        # Define reshape layers
+        self.kernel_reshape = keras.layers.Reshape(
+            target_shape=(
+                height,
+                width,
+                self.kernel_size * self.kernel_size,
+                1,
+                self.group_number,
+            )
+        )
+        self.input_patches_reshape = keras.layers.Reshape(
+            target_shape=(
+                height,
+                width,
+                self.kernel_size * self.kernel_size,
+                num_channels // self.group_number,
+                self.group_number,
+            )
+        )
+        self.output_reshape = keras.layers.Reshape(
+            target_shape=(height, width, num_channels)
+        )
+
+    def call(self, x):
+        # Generate the kernel with respect to the input tensor.
+        # B, H, W, K*K*G
+        kernel_input = self.stride_layer(x)
+        kernel = self.kernel_gen(kernel_input)
+
+        # reshape the kerenl
+        # B, H, W, K*K, 1, G
+        kernel = self.kernel_reshape(kernel)
+
+        # Extract input patches.
+        # B, H, W, K*K*C
+        input_patches = tf.image.extract_patches(
+            images=x,
+            sizes=[1, self.kernel_size, self.kernel_size, 1],
+            strides=[1, self.stride, self.stride, 1],
+            rates=[1, 1, 1, 1],
+            padding="SAME",
+        )
+
+        # Reshape the input patches to align with later operations.
+        # B, H, W, K*K, C//G, G
+        input_patches = self.input_patches_reshape(input_patches)
+
+        # Compute the multiply-add operation of kernels and patches.
+        # B, H, W, K*K, C//G, G
+        output = tf.multiply(kernel, input_patches)
+        # B, H, W, C//G, G
+        output = tf.reduce_sum(output, axis=3)
+
+        # Reshape the output kernel.
+        # B, H, W, C
+        output = self.output_reshape(output)
+
+        # Return the output tensor and the kernel.
+        # return output, kernel
+        return output
